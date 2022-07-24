6b9be58fa18b6e70495c45661ff14f19bd2c2ec7
Lukasz Klimkiewicz <ukasz.klimkiewicz@gmail.com>
2022-07-25 00:20:48 +0200

    Add tree background

--- lib/activities/game/background.rb
+++ lib/activities/game/background.rb
@@ -1,9 +1,18 @@
 require 'gosu'
 
 require_relative '../../utils'
+require_relative '../../framework/window'
 require_relative 'clouds'
 
 class Background
+  LAYER1_SPEED = 2.0
+
+  @layer1_image = Gosu::Image.new('resources/images/layer1.png')
+
+  class << self
+    attr_reader :layer1_image
+  end
+
   def initialize(map)
     @map = map
     @clouds = Clouds.new
@@ -11,10 +20,18 @@ class Background
 
   def draw
     fill_screen(Gosu::Color::WHITE)
+    draw_layer(self.class.layer1_image, LAYER1_SPEED)
     @clouds.draw
   end
 
   def update(elapsed_time)
     @clouds.update(elapsed_time)
+  end
+
+  def draw_layer(image, speed)
+    x1 = (-@map.shift / speed) % Window::WIDTH
+    x2 = x1 - Window::WIDTH
+    image.draw(x1, 0)
+    image.draw(x2, 0)
   end
 end
