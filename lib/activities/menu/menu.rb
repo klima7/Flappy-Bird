b77777747fc51a6299109ef3e474ad087806d7a9
Lukasz Klimkiewicz <ukasz.klimkiewicz@gmail.com>
2022-07-29 23:47:04 +0200

    Add empty menu with background

--- lib/activities/menu/menu.rb
+++ lib/activities/menu/menu.rb
@@ -1,9 +1,22 @@
 require_relative '../../framework/activity'
+require_relative '../common/background'
 
 class MenuActivity < Activity
 
+  BACKGROUND_SPEED = 300
+
+  def initialize
+    @background = Background.new
+    @background_shift = 0
+  end
+
+  def draw
+    @background.draw(@background_shift)
+  end
+
   def update(elapsed_time)
-    puts "update menu"
+    @background.update(elapsed_time)
+    @background_shift += BACKGROUND_SPEED * elapsed_time
   end
 
 end
--- lib/main.rb
+++ lib/main.rb
@@ -1,5 +1,6 @@
-require_relative 'activities/game/game'
+require_relative 'activities/game/game' # TODO: Remove
+require_relative 'activities/menu/menu'
 require_relative 'framework/window'
 
-window = Window.new(GameActivity.new)
+window = Window.new(MenuActivity.new)
 window.show if __FILE__ == $0
